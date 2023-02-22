eb8a3bd3b2aabc0a5c690afaedcab8e103cedac9
Yerong Li <yerong.li@outlook.com>
2023-02-22 15:10:55 -0600

    Use 6 as batch size

--- codes_zcj/RUN/train_vanilla_dialogpt.sh
+++ codes_zcj/RUN/train_vanilla_dialogpt.sh
@@ -1,13 +1,13 @@
-CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py \
+CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 python train.py \
     --config_name vanilla_dialogpt \
     --inputter_name vanilla \
     --eval_input_file ./_reformat/valid.txt \
     --seed 13 \
     --max_input_length 180 \
     --max_decoder_input_length 45 \
-    --train_batch_size 4 \
+    --train_batch_size 6 \
     --gradient_accumulation_steps 1 \
-    --eval_batch_size 4 \
+    --eval_batch_size 6 \
     --learning_rate 5e-5 \
     --num_epochs 2 \
     --warmup_steps 100 \
