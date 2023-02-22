a7fc5c1babca46677ce51d07a35bab97455a7547
Yerong Li <yerong.li@outlook.com>
2023-02-22 15:09:49 -0600

    Use 4 as batch size

--- codes_zcj/RUN/train_vanilla_dialogpt.sh
+++ codes_zcj/RUN/train_vanilla_dialogpt.sh
@@ -5,9 +5,9 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 python train.py \
     --seed 13 \
     --max_input_length 180 \
     --max_decoder_input_length 45 \
-    --train_batch_size 3 \
+    --train_batch_size 4 \
     --gradient_accumulation_steps 1 \
-    --eval_batch_size 3 \
+    --eval_batch_size 4 \
     --learning_rate 5e-5 \
     --num_epochs 2 \
     --warmup_steps 100 \
