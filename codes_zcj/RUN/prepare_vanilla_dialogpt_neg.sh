6b7b8c208c7ed2fefc670e47d4e638f93dca09ab
Yerong Li <yerong.li@outlook.com>
2023-02-23 12:44:24 -0600

    Create prepare_vanilla_dialogpt_neg.sj

---
+++ codes_zcj/RUN/prepare_vanilla_dialogpt_neg.sh
@@ -0,0 +1,6 @@
+python prepare.py \
+    --config_name vanilla_dialogpt \
+    --inputter_name neg \
+    --train_input_file ./_reformat/train.txt \
+    --max_input_length 180 \
+    --max_decoder_input_length 45
