a1f5134e44d7082d82e824894aea55a9749c4bc2
Yerong Li <yerong.li@outlook.com>
2023-02-23 16:04:47 -0600

    train_vanilla_dialogpt_neg.sh use 6 GPUs

--- codes_zcj/RUN/train_vanilla_dialogpt_neg.sh
+++ codes_zcj/RUN/train_vanilla_dialogpt_neg.sh
@@ -1,13 +1,13 @@
-CUDA_VISIBLE_DEVICES=6,7 python train_cls.py \
+CUDA_VISIBLE_DEVICES=2,3,4,5,6,7 python train_cls.py \
     --config_name vanilla_dialogpt_neg \
     --inputter_name vanilla \
     --eval_input_file ./_reformat/valid.txt \
     --seed 13 \
     --max_input_length 180 \
     --max_decoder_input_length 45 \
-    --train_batch_size 1\
+    --train_batch_size 6\
     --gradient_accumulation_steps 1 \
-    --eval_batch_size 1 \
+    --eval_batch_size 6 \
     --learning_rate 5e-5 \
     --num_epochs 2 \
     --warmup_steps 100 \
