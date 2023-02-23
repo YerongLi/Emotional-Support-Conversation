37da8c1fd0dba313e1fb59f4468ca9810abfef7e
Yerong Li <yerong.li@outlook.com>
2023-02-23 14:53:48 -0600

    Revert train.py

--- codes_zcj/RUN/train_vanilla_dialogpt.sh
+++ codes_zcj/RUN/train_vanilla_dialogpt.sh
@@ -1,13 +1,13 @@
-CUDA_VISIBLE_DEVICES=6,7 python train.py \
-    --config_name vanilla_dialogpt \
+CUDA_VISIBLE_DEVICES=6,7 python train_cls.py \
+    --config_name vanilla_dialogpt_neg \
     --inputter_name vanilla \
     --eval_input_file ./_reformat/valid.txt \
     --seed 13 \
     --max_input_length 180 \
     --max_decoder_input_length 45 \
-    --train_batch_size 6 \
+    --train_batch_size 1\
     --gradient_accumulation_steps 1 \
-    --eval_batch_size 6 \
+    --eval_batch_size 1 \
     --learning_rate 5e-5 \
     --num_epochs 2 \
     --warmup_steps 100 \
