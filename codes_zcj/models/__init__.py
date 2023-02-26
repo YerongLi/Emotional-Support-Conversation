fdfbb5246a5c6880fb1ed66f680ea1cb52339613
Yerong Li <yerong.li@outlook.com>
2023-02-26 00:47:04 -0600

    Copy vanilla dialogpt as vanilla_dialog_neg.py

--- codes_zcj/models/__init__.py
+++ codes_zcj/models/__init__.py
@@ -4,6 +4,7 @@ from models.vanilla_blenderbot_small import Model as vanilla_blenderbot_small
 
 from models.strat_dialogpt import Model as strat_dialogpt
 from models.vanilla_dialogpt import Model as vanilla_dialogpt
+from models.vanilla_dialogpt_neg import Model as vanilla_dialogpt_neg
 
 models = {
     
@@ -11,5 +12,6 @@ models = {
     'strat_blenderbot_small': strat_blenderbot_small,
     
     'vanilla_dialogpt': vanilla_dialogpt,
+    'vanilla_dialogpt_neg': vanilla_dialogpt_neg,
     'strat_dialogpt': strat_dialogpt,
 }
