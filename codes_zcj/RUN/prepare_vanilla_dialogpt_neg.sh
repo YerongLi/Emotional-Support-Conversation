f9c12c840b47d93946ddf558540ad5f3a50b0aa3
Yerong Li <yerong.li@outlook.com>
2023-03-02 12:50:15 -0600

    print dev in processed_data

--- codes_zcj/RUN/prepare_vanilla_dialogpt_neg.sh
+++ codes_zcj/RUN/prepare_vanilla_dialogpt_neg.sh
@@ -1,6 +1,6 @@
 python prepare_neg.py \
     --config_name vanilla_dialogpt_neg \
     --inputter_name vanilla \
-    --train_input_file ./_reformat/train.txt \
+    --train_input_file ./_reformat/train_neg.txt \
     --max_input_length 180 \
     --max_decoder_input_length 45
